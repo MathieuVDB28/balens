1a05b37a8bd1d5a83f17f2e39b8b14fbe2e7d4ce
MathieuVDB28 <mathios2000@gmail.com>
2025-04-25 22:23:48 +0200

    initial commit

---
+++ types/next-auth.d.ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+    interface User {
+        id: string;
+        name?: string;
+        email: string;
+        image?: string;
+    }
+
+    interface Session {
+        user: User;
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        id: string;
+    }
+}
